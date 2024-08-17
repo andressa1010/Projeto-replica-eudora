b5880bcd9a1ac56eba05dbf93405814420e30f2f
andressa1010 <andressa.ns10@gmail.com>
2024-08-16 21:09:58 -0300

    novas-atualizacoes

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -5,19 +5,25 @@ const Sobre = () => {
   return (
     <>
       <Menu />
-      <div className="sobre-log">
-        <p>
-          Olá! Meu nome é Andressa, e sou desenvolvedora web. Este site é uma
+
+       <div className="container-sobre">
+        <img src="/img.andressa3.jpg" alt="Descrição da Imagem" className="image"></img>
+        <div className="text">
+          <h2>Olá! Meu nome é Andressa, e sou desenvolvedora web.</h2>
+        <p>Este site é uma
           réplica do site da Eudora, criado como parte do meu portfólio para
           demonstrar minhas habilidades em desenvolvimento front-end. Tenho
           experiência em JavaScript React Html Css e estou sempre em busca de
-          novos desafios e oportunidades para crescer profissionalmente.
-        </p>
-        <p>
-          Se você quiser saber mais sobre mim ou sobre meus projetos, sinta-se à
-          vontade para entrar em contato!
-        </p>
-      </div>
+          novos desafios e oportunidades para crescer profissionalmente.</p>
+
+          <img src="/icons8-javascript-48.png" alt="logo"></img>
+        <img src="/icons8-react-48.png" alt="logo"></img>
+        <img src="/icons8-html-48.png" alt="logo"></img>
+        <img src="/icons8-css-48.png" alt="logo"></img>
+        </div>
+        </div>
+   
+      
     </>
   );
 };
